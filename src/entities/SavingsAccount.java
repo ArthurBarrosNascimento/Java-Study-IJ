0342099dcf4ea3943a08072755eeb0ab65b618f8
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-12-05 23:37:01 -0300

    class final method

--- src/entities/SavingsAccount.java
+++ src/entities/SavingsAccount.java
@@ -1,6 +1,6 @@
 package entities;
 
-public class SavingsAccount extends Account{
+public final class SavingsAccount extends Account{
     private Double interestRate;
     public SavingsAccount() {
         super();
