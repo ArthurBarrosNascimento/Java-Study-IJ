14a6f9528eee838057713f8fe225c6fa0a8ac469
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-12-13 14:46:17 -0300

    starting to study: try-with-resources block

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String path = "C:\\Users\\arthu\\Documents\\in.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
             String line = br.readLine();
 
             while (line != null) {
@@ -24,18 +19,6 @@ public class Main {
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        } finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
-
     }
 }   
