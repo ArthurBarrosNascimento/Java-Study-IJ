88e0d42259924fa1817a4d5df9f7195a84dc543e
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-12-13 14:42:05 -0300

    starting to study: FileReader and BufferedReader

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -6,13 +8,33 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        File file = new File("C:\\Users\\arthu\\Documents\\in.txt");
-        try (Scanner sc = new Scanner(file)) {
-            while (sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
+        String path = "C:\\Users\\arthu\\Documents\\in.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
     }
