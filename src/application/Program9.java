72dcca3fa9e44bb9725bb15ae8874c49c2dc4c05
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-12-20 17:28:42 -0300

    studying: Set - Solved exercise

---
+++ src/application/Program9.java
@@ -0,0 +1,41 @@
+package application;
+
+import entities.LogEntry;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Date;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Program9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter file full path: ");
+        String path = sc.nextLine();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            Set<LogEntry> set = new HashSet<>();
+
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(" ");
+                String username = fields[0];
+                Date moment = Date.from(Instant.parse(fields[1]));
+
+                set.add(new LogEntry(username, moment));
+
+                line = br.readLine();
+            }
+
+            System.out.println("Total users: " + set.size());
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
