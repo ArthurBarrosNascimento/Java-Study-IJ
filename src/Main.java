2ec0244f2d407e8941b1bbdb8a04ebade78217b5
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-12-20 16:15:44 -0300

    studying: Set - TreeSet
    slower (O(Log(n)) operations in red-black tree) and ordered by the object's compareTo (or Comparator)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        Set<String> set = new HashSet<>();
+        Set<String> set = new TreeSet<>();
 
         set.add("TV");
         set.add("Notebook");
