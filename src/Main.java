8aa4e894b7107e5184cfcf59d8babdd162c01590
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-12-20 16:24:14 -0300

    studying: Set - testing some important methods

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,24 @@
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Set<String> set = new LinkedHashSet<>();
+        Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
+        Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
 
-        set.add("TV");
-        set.add("Notebook");
-        set.add("Tablet");
+        //union
+        Set<Integer> c = new TreeSet<>(a);
+        c.addAll(b);
+        System.out.println(c);
 
-        System.out.println(set.contains("Notebook"));
+        //intersection
+        Set<Integer> d = new TreeSet<>(a);
+        d.retainAll(b);
+        System.out.println(d);
 
-        for (String p : set) {
-            System.out.println(p);
-        }
+        //difference
+        Set<Integer> e = new TreeSet<>(a);
+        e.removeAll(b);
+        System.out.println(e);
     }
 }   
