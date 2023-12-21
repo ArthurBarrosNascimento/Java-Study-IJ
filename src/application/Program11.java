7fb2b1b56f8a88527e2aa151b112e7dfe3f9992b
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-12-21 17:50:31 -0300

    studying: Predicate - Declared lambda expression

--- src/application/Program11.java
+++ src/application/Program11.java
@@ -1,10 +1,10 @@
 package application;
 
 import entities.Product;
-import util.ProductPredicate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Program11 {
     public static void main(String[] args) {
@@ -15,7 +15,9 @@ public class Program11 {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.removeIf(Product::nonStaticProductPredicate);
+        Predicate<Product> pred = p -> p.getPrice() >= 100;
+
+        list.removeIf(pred);
 
         for (Product p: list) {
             System.out.println(p.getName() + ": " + p.getPrice());
