3cade96dc4db267ce7cfdb9c601d71209dbe5ed0
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-12-21 17:04:18 -0300

    studying: Predicate

---
+++ src/application/Program11.java
@@ -0,0 +1,24 @@
+package application;
+
+import entities.Product;
+import util.ProductPredicate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program11 {
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        list.removeIf(new ProductPredicate());
+
+        for (Product p: list) {
+            System.out.println(p.getName() + ": " + p.getPrice());
+        }
+    }
+}
