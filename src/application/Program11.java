d02a446fc0533237c630c678c75741b304f64e64
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-12-21 19:02:06 -0300

    studying: Function - Inline lambda expression

--- src/application/Program11.java
+++ src/application/Program11.java
@@ -1,11 +1,9 @@
 package application;
 
 import entities.Product;
-import util.UpperCaseName;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 
 public class Program11 {
     public static void main(String[] args) {
@@ -16,9 +14,7 @@ public class Program11 {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        Function<Product, String> func = p -> p.getName().toUpperCase();
-
-        List<String> names = list.stream().map(func).toList();
+        List<String> names = list.stream().map(p -> p.getName().toUpperCase()).toList();
 
         names.forEach(System.out::println);
     }
