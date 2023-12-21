6c49ad42ec838fd914fc7c2aa72958613589bc66
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-12-21 14:24:30 -0300

    studying: Comparator
    Now we have an anonymous function, which would be a lambda expression without curly braces

--- src/application/Program8.java
+++ src/application/Program8.java
@@ -12,11 +12,7 @@ public class Program8 {
         list.add(new Product("Notebook", 1200.0));
         list.add(new Product("Tablet", 400.0));
 
-        Comparator<Product> comp = (p1, p2) -> {
-            return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-        };
-
-        list.sort(comp);
+        list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 
         for (Product p : list) {
             System.out.println(p);
