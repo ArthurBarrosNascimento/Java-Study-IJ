9a4300e2362afc09cb29595fca663ecc843db009
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-12-21 18:39:08 -0300

    studying: Consumer - Declared lambda expression

--- src/application/Program11.java
+++ src/application/Program11.java
@@ -1,10 +1,10 @@
 package application;
 
 import entities.Product;
-import util.PriceUpdate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Program11 {
     public static void main(String[] args) {
@@ -15,7 +15,9 @@ public class Program11 {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.forEach(Product::nonStaticVoidPriceUpdate);
+        Consumer<Product> cons = p -> p.setPrice(p.getPrice() * 1.1);
+
+        list.forEach(cons);
 
         list.forEach(System.out::println);
     }
