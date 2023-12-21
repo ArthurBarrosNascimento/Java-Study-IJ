9b0c8ac6ebccf703559828974783bedd8bd8b11c
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-12-21 17:48:23 -0300

    studying: Predicate - Reference method with non-static method

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -44,6 +44,10 @@ public class Product {
         return p.getPrice() >= 100;
     }
 
+    public boolean nonStaticProductPredicate() {
+        return price >= 100;
+    }
+
     @Override
     public String toString() {
         return "Product{" +
