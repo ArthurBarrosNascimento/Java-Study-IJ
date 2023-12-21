9454fe4a5b96fd9f246ba6e0d66471086845f0d4
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-12-21 14:12:54 -0300

    studying: Comparator
    Now we implement one of the ways to use the Comparator in the list.sort() method, which is to create an anonymous class

--- src/application/Program8.java
+++ src/application/Program8.java
@@ -12,14 +12,14 @@ public class Program8 {
         list.add(new Product("Notebook", 1200.0));
         list.add(new Product("Tablet", 400.0));
 
-//        Comparator<Product> comp = new Comparator<Product>() {
-//            @Override
-//            public int compare(Product p1, Product p2) {
-//                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-//            }
-//        };
+        Comparator<Product> comp = new Comparator<Product>() {
+            @Override
+            public int compare(Product p1, Product p2) {
+                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+            }
+        };
 
-        list.sort(new MyComparator());
+        list.sort(comp);
 
         for (Product p : list) {
             System.out.println(p);
